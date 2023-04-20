5936e31d1c4db1abb68062f56847e1bff762cd4a
Hector <torrezhectorb@gmail.com>
2023-04-19 20:08:22 -0600

    edit state

--- src/components/ExerciseForm/ExerciseForm.jsx
+++ src/components/ExerciseForm/ExerciseForm.jsx
@@ -1,132 +1,99 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const ExerciseForm = ({ muscle }) => {
-
-  const [exercises, setExercises] = useState([{
-    id: new Date().getTime(),
-    exercise: "",
-    sets: "",
-    reps: "",
-    weight: "",
-  }])
-
-
-
-  const [isEdit, setIsEdit] = useState(true)
-  const [showContent, setShowContent] = useState(true)
-
-  const togleContent = () => {
-    setShowContent(!showContent)
-  }
-
-  const handleChange = (event) => {
-    const { name, value } = event.target
-    setExercises([{ ...exercises, [name]: value }])
-  }
-
-  const handleSave = (id) => {
-    if (exercises.find(ex => ex.id !== id)) {
-      setIsEdit(false)
-
-    }
-
-  }
-
-  const handleAddExercise = () => {
-    setExercises([...exercises, {
-      id: new Date().getTime(),
-      muscle: muscle,
-      exercise: "",
-      sets: "",
-      reps: "",
-      weight: "",
-    }])
-  }
-
-  const handleEdit = () => {
-    setIsEdit(true)
-  }
-
-  console.log(exercises)
+export const ExerciseForm = ({ muscles, onAddMuscle }) => {
+  const [exercise, setExercise] = useState("");
+  const [isEdit, setIsEdit] = useState(true);
 
   return (
     <>
-      <p onClick={togleContent}>Press me for check</p>
+      <section className="bg-slate-100  shadow-md rounded px-1 pt-1 pb-1 mb-4">
+        <>
+          <div>
+            <button>Add Exercise</button>
+          </div>
 
-      {
+          <div className="bg-white   rounded  mb-1" key={muscles.id}>
+            <div className="">
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="exercise"
+              >
+                Exercice
+              </label>
+              {isEdit ? (
+                <input
+                  className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  name="exercise"
+                  value={exercise}
+                  onChange={(event) => setExercise(event.target.value)}
+                />
+              ) : (
+                <span>{exercise}</span>
+              )}
 
-        muscle.map(mus => (
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="sets"
+              >
+                Sets
+              </label>
+              {isEdit ? (
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="number"
+                  name="sets"
+                  // value={sets}
+                  // onChange={handleChange}
+                />
+              ) : (
+                <span>{sets}</span>
+              )}
 
-          <section className="bg-slate-100  shadow-md rounded px-1 pt-1 pb-1 mb-4" key={mus}>
-            <h2 className="text-xl text-center mb-4">{mus}</h2>
-            {
-              showContent && (
-                <>
-                  <div>
-                    <button onClick={handleAddExercise}>
-                      Add Exercise
-                    </button>
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="reps"
+              >
+                Reps
+              </label>
+              {isEdit ? (
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="number"
+                  name="reps"
+                  // value={reps}
+                  // onChange={handleChange}
+                />
+              ) : (
+                <span>{reps}</span>
+              )}
 
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="weight"
+              >
+                Weight
+              </label>
+              {isEdit ? (
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="number"
+                  name="weight"
+                  // value={weight}
+                  // onChange={handleChange}
+                />
+              ) : (
+                <span>{weight}</span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <button>Save</button>
 
-                  </div>
-                  {
-
-                    exercises.map(ex => (
-
-                      <div className="bg-white   rounded  mb-1" key={ex.id}>
-                        <div className="">
-
-                          <label className="block text-gray-700 font-bold mb-2" htmlFor="excercise">Excersice</label>
-                          {isEdit ? (
-                            <input className="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="exercise" value={ex.exercise} onChange={handleChange} />
-                          ) : (<span>{ex.exercise}</span>)}
-
-                          <label className="block text-gray-700 font-bold mb-2" htmlFor="sets">Sets</label>
-                          {
-                            isEdit ? (
-                              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" name="sets" value={ex.sets} onChange={handleChange} />
-                            ) : (<span>{ex.sets}</span>)
-                          }
-
-                          <label className="block text-gray-700 font-bold mb-2" htmlFor="reps">Reps</label>
-                          {
-                            isEdit ? (
-                              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" name="reps" value={ex.reps} onChange={handleChange} />
-                            ) : (<span>{ex.reps}</span>)
-                          }
-
-                          <label className="block text-gray-700 font-bold mb-2" htmlFor="weight">Weight</label>
-                          {
-                            isEdit ? (
-                              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" name="weight" value={ex.weight} onChange={handleChange} />
-                            ) : (<span>{ex.weight}</span>)
-                          }
-
-                        </div>
-                        <div className="flex flex-col">
-                          <button onClick={() => handleSave(ex.id)}>
-                            Save
-                          </button>
-
-                          <button onClick={handleEdit}>
-                            Edit
-                          </button>
-
-
-                        </div>
-                      </div>
-
-                    ))
-                  }
-                </>
-              )
-            }
-
-
-          </section>
-        ))
-      }
-
+              <button>Edit</button>
+            </div>
+          </div>
+        </>
+      </section>
     </>
-  )
-}
+  );
+};
--- src/components/WorkoutForm/WorkoutForm.jsx
+++ src/components/WorkoutForm/WorkoutForm.jsx
@@ -1,61 +1,117 @@
-import { useState } from "react"
-import { ExerciseForm } from "../ExerciseForm/ExerciseForm"
+import { useEffect, useState } from "react";
+import { ExerciseForm } from "../ExerciseForm/ExerciseForm";
+
+const initialExercises = [
+  {
+    id: new Date().getTime(),
+    muscle: "",
+    exercises: [
+      {
+        id: new Date().getTime() * 5,
+        sets: "",
+        reps: "",
+        weight: "",
+      },
+    ],
+  },
+];
 
 export const WorkoutForm = () => {
+  const [muscles, setMuscles] = useState(initialExercises);
+  const [muscleInput, setMuscleInput] = useState("");
+  const [showExerciseForm, setShowExerciseForm] = useState(false);
+  const [alert, setAlert] = useState(false);
 
-  const [muscle, setMuscle] = useState([])
-  const [muscleInput, setMuscleInput] = useState('')
-  const [showExerciseForm, setShowExerciseForm] = useState(false)
-  const [alert, setAlert] = useState(false)
+  console.log(muscles);
 
   const handleDown = (event) => {
-    if (event.key === 'Enter') {
-      handleSubmit()
+    if (event.key === "Enter") {
+      handleClick();
     }
-  }
+  };
 
-  const handleClear = () => {
-    setMuscle([])
-    setShowExerciseForm(false)
-  }
+  const handleAddMuscle = (muscle) => {
+    setMuscles([
+      ...muscles,
+      {
+        id: new Date().getTime(),
+        muscle: muscle,
+        exercises: [
+          {
+            id: new Date().getTime() * 5,
+            sets: "add here",
+            reps: "add here",
+            weight: "add here",
+          },
+        ],
+      },
+    ]);
 
-  const handleSubmit = () => {
-    if (muscleInput === '') return
-    if (muscle.includes(muscleInput)) {
-      setAlert(true)
-      return
-    }
-    setMuscle([...muscle, muscleInput])
-    setShowExerciseForm(true)
-    setMuscleInput('')
-    setAlert(false)
-  }
+    setShowExerciseForm(true);
+  };
+
+  const handleClick = () => {
+    setMuscleInput("");
+    handleAddMuscle(muscleInput);
+  };
+
+  const handleChangeMuscle = (nextMuscle) => {
+    setMuscles(
+      muscles.map((muscle) => {
+        if (muscle.id === nextMuscle.id) {
+          return nextMuscle;
+        } else {
+          return muscle;
+        }
+      })
+    );
+  };
 
   return (
     <section className="max-w-md mx-auto">
-
-      {
-        alert && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-5">
+      {alert && (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-5">
           <p className="text-red-500">Muscle already exists</p>
         </div>
-      }
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="">Muscle</label>
-      <input className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Add Muscle" type="text" name="muscle" value={muscleInput} onChange={(event) => setMuscleInput(event.target.value)} onKeyDown={handleDown} />
+      )}
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleSubmit}>
+      <label className="block text-gray-700 font-bold mb-2" htmlFor="">
+        Muscle
+      </label>
+      <input
+        className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        placeholder="Add Muscle"
+        type="text"
+        name="muscle"
+        value={muscleInput}
+        onChange={(event) => setMuscleInput(event.target.value)}
+        onKeyDown={handleDown}
+      />
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleClick}
+      >
         Add Muscle
       </button>
 
-      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleClear}>
+      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
         Clear
       </button>
 
-      {
-        showExerciseForm &&
-        <ExerciseForm muscle={muscle} />
+      {showExerciseForm &&
+        muscles.map((muscle) => (
+          <section key={muscle.id}>
+            <h2>{muscle.muscle}</h2>
 
-      }
-
+            <ExerciseForm
+              muscles={muscles}
+              onChange={handleChangeMuscle}
+              onAddMuscle={handleAddMuscle}
+            />
+          </section>
+        ))}
+      {/* {showExerciseForm && <ExerciseForm muscle={muscle} />} */}
     </section>
-  )
-}
+  );
+};
